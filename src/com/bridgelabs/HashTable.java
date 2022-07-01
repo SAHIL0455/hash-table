b34c40d25d8bccba0c724ce72d32dd6cb671877c
SAHIL0455 <sahilpawar1004@gmail.com>
2022-07-01 08:55:05 +0530

    ADDED: Ability to find frequency of word in a sentence

--- src/com/bridgelabs/HashTable.java
+++ src/com/bridgelabs/HashTable.java
@@ -1,7 +1,28 @@
 package com.bridgelabs;
 
+import java.util.Hashtable;
+
 public class HashTable {
     public static void main(String[] args) {
         System.out.println("Welcome to HashTable");
+        String message = "To be or not to be";
+        HashTable hashTable = new HashTable();
+        hashTable.frequency(message);
+        System.out.println(hashtable);
+    }
+
+    public static Hashtable<String, Integer> hashtable = new Hashtable<>();
+
+    public void frequency(String sentances) {
+        String[] messageArray = sentances.toLowerCase().split(" ");
+        for (String word : messageArray) {
+            Integer value = hashtable.get(word);
+
+            if (value == null)
+                value = 1;
+            else
+                value = value + 1;
+            hashtable.put(word, value);
+        }
     }
 }
